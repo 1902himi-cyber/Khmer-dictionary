301eb19e5fdac2cb9803b815de3bafc0ce2131a2
1902himi-cyber <1902himi@gmail.com>
2026-07-07 14:55:17 +0900

    Fix formatting and add alerts in script.js

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-window.onload = async function(){
+window.onload = async function () {
+
+    alert("① script.js 開始");
 
     await loadDictionary();
 
+    alert("② 辞書件数：" + dict.length);
+
     document
       .getElementById("search")
-      .addEventListener("input",searchWord);
+      .addEventListener("input", searchWord);
 
+    alert("③ 準備完了");
 };
